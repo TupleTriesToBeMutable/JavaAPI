009dbe5b50bab689c5c2edb372c9f8bb602c38c7
TupleSosok <jokylionplay@gmail.com>
2024-05-01 15:19:17 +0300

    Добавление swagger

--- src/main/java/jokylionplay/project2024/Project2024Application.java
+++ src/main/java/jokylionplay/project2024/Project2024Application.java
@@ -1,16 +1,12 @@
 package jokylionplay.project2024;
 
-import jokylionplay.project2024.repository.TaskRepository;
-import jokylionplay.project2024.services.TaskService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Project2024Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Project2024Application.class, args);
 
     }
-
 }
