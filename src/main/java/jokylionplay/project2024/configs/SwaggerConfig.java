da86e22a4b3934b6142fe1700d6eb69d8bab113a
TupleSosok <jokylionplay@gmail.com>
2024-05-02 20:38:20 +0300

    Push

--- src/main/java/jokylionplay/project2024/configs/SwaggerConfig.java
+++ src/main/java/jokylionplay/project2024/configs/SwaggerConfig.java
@@ -7,10 +7,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI().info( new Info().title("InternshipApi").version("1.0.0"));
     }
-
 }
