8f7063d6f7af02e32fde50a6f2578f41fd5f2276
TupleSosok <jokylionplay@gmail.com>
2024-05-08 18:01:32 +0300

    Проверка на существования к ... Логика получение успеваемости для пользователя и конкретной стажировки

--- src/main/java/jokylionplay/project2024/services/UserService.java
+++ src/main/java/jokylionplay/project2024/services/UserService.java
@@ -4,10 +4,10 @@ import jokylionplay.project2024.dto.InternshipLessonsDTO;
 import jokylionplay.project2024.dto.UserInfoDTO;
 import jokylionplay.project2024.dto.UserProgressDTO;
 import jokylionplay.project2024.entities.User;
-import jokylionplay.project2024.entities.UserProgress;
 import jokylionplay.project2024.mappers.InternshipMapper;
 import jokylionplay.project2024.mappers.UserMapper;
 import jokylionplay.project2024.mappers.UserProgressMapper;
+import jokylionplay.project2024.repository.InternshipRepository;
 import jokylionplay.project2024.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +21,9 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private InternshipRepository internshipRepository;
 
     public UserInfoDTO getInfo(Long id) throws IllegalArgumentException{
         Optional<User> user = userRepository.findById(id);
@@ -50,7 +53,11 @@ public class UserService {
     }
 
 
-    public List<UserProgressDTO> getReportByInternshipIdAndUserId(Long internshipId, Long userId){
+    public List<UserProgressDTO> getReportByInternshipIdAndUserId(Long internshipId, Long userId) throws IllegalArgumentException{
+        if (!internshipRepository.existsById(internshipId))
+            throw new IllegalArgumentException("Reporting : Intenship dosn`t exist");
+        if(!userRepository.existsById(userId))
+            throw new IllegalArgumentException("Reporting : User dosn`t exist");
         List<Object[]> result = userRepository.getReportByInternshipIdAndUserId(internshipId, userId);
         return UserProgressMapper.MAPPER.toDTOFromListObject(result);
     }
