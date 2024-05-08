40ffce83a4da957c96557da395a316b9ed652893
TupleSosok <jokylionplay@gmail.com>
2024-05-08 19:12:16 +0300

    Добавление пустого контроллера для проверки заданий

--- src/main/java/jokylionplay/project2024/controllers/admin_controllers/CheckingTasksController.java
+++ src/main/java/jokylionplay/project2024/controllers/admin_controllers/CheckingTasksController.java
@@ -1,4 +1,14 @@
 package jokylionplay.project2024.controllers.admin_controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Admin Проверка заданий", description = "Решено или не решено, оставление комментария")
+@RestController
+@RequestMapping("/admin/{adminId}/user/{userId}")
+@AllArgsConstructor
 public class CheckingTasksController {
+
 }
